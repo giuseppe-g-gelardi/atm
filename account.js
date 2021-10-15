fed403939e290912d9df89b8a7ef7dd2f389e7ca
n-rdy <giuseppegelardi@icloud.com>
2021-10-14 20:34:22 -0500

    set up getBalance, working on validatePin

---
+++ account.js
@@ -0,0 +1,8 @@
+const pin = 3825
+
+let balance = 1000
+
+module.exports = {
+  balance,
+  pin,
+}
--- atm.js
+++ atm.js
@@ -1,8 +1,31 @@
+const prompt = require('prompt-sync')
+const { balance, pin } = require('./account')
+
 
 // ! getBalance()
+
+const getBalance = (accountBalance) => {
+  console.log(accountBalance)
+  return accountBalance
+}
+getBalance(balance)
 
 // ! withdraw()
 
 // ! deposit()
 
 // ! validatePin
+
+// let userInputPin = prompt()
+const validatePin = (userPin) => {
+  console.log('what is your pin?')
+  let userInputPin = prompt()
+  let count = 0
+  while (userInputPin !== userPin) {
+    console.log('that is not the correct pin')
+    count++
+  }
+  
+}
+validatePin(pin)
+
